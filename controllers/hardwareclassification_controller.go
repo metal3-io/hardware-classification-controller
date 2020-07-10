2187bf7d8ad75f5d4d4f71fd412c94d2cc95ae0c
Ravi Waghmare <raviwaghmare2908@gmail.com>
2020-07-10 17:54:58 +0530

    Initialized the logger with namespace

--- controllers/hardwareclassification_controller.go
+++ controllers/hardwareclassification_controller.go
@@ -49,6 +49,9 @@ type HardwareClassificationReconciler struct {
 func (hcReconciler *HardwareClassificationReconciler) Reconcile(req ctrl.Request) (_ ctrl.Result, reterr error) {
 	ctx := context.Background()
 
+	// Initialize the logger with namespace
+	hcReconciler.Log = hcReconciler.Log.WithName(HWControllerName).WithValues("metal3-hardwareclassification", req.NamespacedName)
+
 	// Get HardwareClassificationController to get values for Namespace and ExpectedHardwareConfiguration
 	hardwareClassification := &hwcc.HardwareClassification{}
 
