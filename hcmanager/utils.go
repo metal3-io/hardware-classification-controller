f27ee492252cacd25e9fbffc576ce3a77117d4a2
Ravi Waghmare <raviwaghmare2908@gmail.com>
2020-07-13 16:58:24 +0530

    Updated import sequence

--- hcmanager/utils.go
+++ hcmanager/utils.go
@@ -18,8 +18,9 @@ package hcmanager
 import (
 	"context"
 	"errors"
-	hwcc "hardware-classification-controller/api/v1alpha1"
 	"net"
+
+	hwcc "hardware-classification-controller/api/v1alpha1"
 
 	bmh "github.com/metal3-io/baremetal-operator/pkg/apis/metal3/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
