965a271cbe668b17449f3349f96de3c746c39435
Neha Alhat <neha.alhat.30@gmail.com>
2020-09-03 16:49:38 +0530

    Modified test cases

--- hcmanager/utils_test.go
+++ hcmanager/utils_test.go
@@ -30,43 +30,97 @@ import (
 
 var _ = Describe("HCManager", func() {
 
+	type testCaseFetchBMH struct {
+		namespace     string
+		hosts         []runtime.Object
+		expectedError bool
+	}
+
 	DescribeTable("Test fetch BaremetalHost list",
-		func(namespace string) {
-			hostTest := getHosts()
-			c := fakeclient.NewFakeClientWithScheme(setupSchemeMm(), hostTest...)
+		func(tc testCaseFetchBMH) {
+			c := fakeclient.NewFakeClientWithScheme(setupSchemeMm(), tc.hosts...)
 			hcManager := NewHardwareClassificationManager(c, klogr.New())
-			result, _, err := hcManager.FetchBmhHostList(namespace)
-			if err != nil {
+			result, _, err := hcManager.FetchBmhHostList(tc.namespace)
+			if tc.expectedError {
 				Expect(len(result)).To(BeZero())
-			}
-			if len(result) > 0 {
-				Expect(len(result)).Should(Equal(2))
+			} else {
+				Expect(err).NotTo(HaveOccurred())
+				Expect(len(result)).NotTo(BeZero())
 			}
 		},
-		Entry("Should fetch BaremetalHosts in ready state and under metal3 namespace", getNamespace()),
-		Entry("Should return error while fetching BaremetalHosts", "sample"),
+		Entry("Should fetch BaremetalHosts in ready state and under metal3 namespace",
+			testCaseFetchBMH{
+				namespace:     getNamespace(),
+				hosts:         getHosts(),
+				expectedError: false},
+		),
+		Entry("Should return error while fetching BaremetalHosts",
+			testCaseFetchBMH{
+				namespace:     "sample",
+				hosts:         getHosts(),
+				expectedError: true},
+		),
 	)
 
+	type testCaseValidation struct {
+		hosts         []runtime.Object
+		hwcProfile    hwcc.HardwareCharacteristics
+		expectedError bool
+	}
+
 	DescribeTable("Test user profiles",
-		func(hwcc hwcc.HardwareCharacteristics) {
-			hostTest := getHosts()
-			c := fakeclient.NewFakeClientWithScheme(setupSchemeMm(), hostTest...)
+		func(tc testCaseValidation) {
+			c := fakeclient.NewFakeClientWithScheme(setupSchemeMm(), tc.hosts...)
 			hcManager := NewHardwareClassificationManager(c, klogr.New())
-			err := hcManager.ValidateExtractedHardwareProfile(hwcc)
-			if err != nil {
+			err := hcManager.ValidateExtractedHardwareProfile(tc.hwcProfile)
+			if tc.expectedError {
 				Expect(err).To(HaveOccurred())
 			} else {
-				Expect(err).Should(BeNil())
+				Expect(err).NotTo(HaveOccurred())
 			}
-
 		},
-		Entry("Test Valid Profile", getExtractedHardwareProfile()),
-		Entry("Test Empty profile", getEmptyProfile()),
-		Entry("Test Invalid CPU details", getInvalidCPUProfile()),
-		Entry("Test Invalid DISK details", getInvalidDiskProfile()),
-		Entry("Test Invalid RAM details", getInvalidRAMProfile()),
-		Entry("Test Invalid NIC details", getInvalidNicProfile()),
-		Entry("Test Missing NIC details", getMissingNicDetails()),
+		Entry("Test Valid Profile",
+			testCaseValidation{
+				hosts:         getHosts(),
+				hwcProfile:    getExtractedHardwareProfile(),
+				expectedError: false},
+		),
+		Entry("Test Empty Profile",
+			testCaseValidation{
+				hosts:         getHosts(),
+				hwcProfile:    getEmptyProfile(),
+				expectedError: true},
+		),
+		Entry("Test Invalid CPU Details",
+			testCaseValidation{
+				hosts:         getHosts(),
+				hwcProfile:    getInvalidCPUProfile(),
+				expectedError: true},
+		),
+		Entry("Test Invalid DISK Details",
+			testCaseValidation{
+				hosts:         getHosts(),
+				hwcProfile:    getInvalidDiskProfile(),
+				expectedError: true},
+		),
+		Entry("Test Invalid RAM Details",
+			testCaseValidation{
+				hosts:         getHosts(),
+				hwcProfile:    getInvalidRAMProfile(),
+				expectedError: true},
+		),
+		Entry("Test Invalid NIC Details",
+			testCaseValidation{
+				hosts:         getHosts(),
+				hwcProfile:    getInvalidNicProfile(),
+				expectedError: true},
+		),
+		Entry("Test Missing NIC Details",
+			testCaseValidation{
+				hosts:         getHosts(),
+				hwcProfile:    getMissingNicDetails(),
+				expectedError: false},
+		),
 	)
 })
 
