77c99b1f9743ec36412424a237be4478900b7181
Doug Hellmann <dhellmann@redhat.com>
2020-10-29 18:14:42 -0400

    make the host controller watch classification resources
    Add a watch for classification resources so when one changes we
    re-enqueue all of the hosts to check them against the new rules.
    
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- controllers/baremetalhost_controller.go
+++ controllers/baremetalhost_controller.go
@@ -17,13 +17,18 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	bmh "github.com/metal3-io/baremetal-operator/apis/metal3.io/v1alpha1"
+	hwcc "github.com/metal3-io/hardware-classification-controller/api/v1alpha1"
 )
 
 // BareMetalHostReconciler reconciles a BareMetalHost object
@@ -35,15 +40,53 @@ type BareMetalHostReconciler struct {
 
 func (r *BareMetalHostReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	_ = context.Background()
-	_ = r.Log.WithValues("baremetalhost", req.NamespacedName)
+	logger := r.Log.WithValues("baremetalhost", req.NamespacedName)
 
-	// your logic here
+	logger.Info("changed")
 
 	return ctrl.Result{}, nil
 }
 
 func (r *BareMetalHostReconciler) SetupWithManager(mgr ctrl.Manager) error {
+
+	mapper := hostMapper{
+		client: mgr.GetClient(),
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&bmh.BareMetalHost{}).
+		Named("baremetalhost").
+		Watches(&source.Kind{Type: &hwcc.HardwareClassification{}},
+			&handler.EnqueueRequestsFromMapFunc{ToRequests: &mapper}).
 		Complete(r)
+}
+
+type hostMapper struct {
+	client client.Client
+}
+
+func (m *hostMapper) Map(obj handler.MapObject) []ctrl.Request {
+	log := ctrl.Log.WithName("controllers").WithName("BareMetalHost").WithName("mapper").
+		WithValues("HardwareClassification",
+			fmt.Sprintf("%s/%s", obj.Meta.GetNamespace(), obj.Meta.GetName()))
+
+	bmhHostList := bmh.BareMetalHostList{}
+	opts := &client.ListOptions{}
+	err := m.client.List(context.TODO(), &bmhHostList, opts)
+	if err != nil {
+		log.Error(err, "could not fetch host list")
+		return nil
+	}
+
+	requests := []ctrl.Request{}
+	for _, host := range bmhHostList.Items {
+		log.Info("found host", "name", host.Name)
+		requests = append(requests, ctrl.Request{
+			NamespacedName: types.NamespacedName{
+				Name:      host.Name,
+				Namespace: host.Namespace,
+			},
+		})
+	}
+	return requests
 }
