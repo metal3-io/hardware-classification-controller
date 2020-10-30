7cf4b55372f2d6f59fa1895ace30676e542fe22f
Doug Hellmann <dhellmann@redhat.com>
2020-10-30 17:47:09 -0400

    enable logging in tests
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- classifier/classifier_test.go
+++ classifier/classifier_test.go
@@ -4,7 +4,15 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
+
+func init() {
+	ctrl.SetLogger(zap.New(func(o *zap.Options) {
+		o.Development = true
+	}))
+}
 
 func TestCheckRangeInt(t *testing.T) {
 	assert.True(t, checkRangeInt(0, 0, 99))
