40d5505003c437bf2f1a7a94c2d3f9f4623aaa70
Ashu Kumar <ashughorla@gmail.com>
2020-12-30 16:47:24 +0530

    NIC Vendor parameter.

--- classifier/nic.go
+++ classifier/nic.go
@@ -1,22 +1,24 @@
 package classifier
 
 import (
+	_ "fmt"
 	bmh "github.com/metal3-io/baremetal-operator/apis/metal3.io/v1alpha1"
-
 	hwcc "github.com/metal3-io/hardware-classification-controller/api/v1alpha1"
+	"strings"
 )
 
 func checkNICs(profile *hwcc.HardwareClassification, host *bmh.BareMetalHost) bool {
 	nicDetails := profile.Spec.HardwareCharacteristics.Nic
+	var nicVendors []string
 	if nicDetails == nil {
 		return true
 	}
-
 	ok := checkRangeInt(
 		nicDetails.MinimumCount,
 		nicDetails.MaximumCount,
 		len(host.Status.HardwareDetails.NIC),
 	)
+
 	log.Info("NIC",
 		"host", host.Name,
 		"profile", profile.Name,
@@ -27,5 +29,48 @@ func checkNICs(profile *hwcc.HardwareClassification, host *bmh.BareMetalHost) bo
 		"ok", ok,
 	)
 
+	if nicDetails.NicSelector == nil {
+		return ok
+	}
+
+	for i := 0; i < len(host.Status.HardwareDetails.NIC); i++ {
+		vendorProduct := strings.Fields(host.Status.HardwareDetails.NIC[i].Model)
+		nicVendors = append(nicVendors, vendorProduct[0])
+	}
+	ok = checkVendor(
+		nicVendors,
+		nicDetails.NicSelector.Vendor,
+	)
+
+	log.Info("NIC",
+		"host", host.Name,
+		"profile", profile.Name,
+		"namespace", host.Namespace,
+		"Require Nics vendor", nicDetails.NicSelector.Vendor,
+		"Actual Nics Vendor", nicVendors,
+		"ok", ok,
+	)
+
 	return ok
+}
+
+func checkVendor(actualNicVendors, requireNicVendor []string) bool {
+	if len(requireNicVendor) > len(actualNicVendors) {
+		return false
+	}
+	nicCount := len(requireNicVendor)
+	for _, requireNic := range requireNicVendor {
+		for _, actualNic := range actualNicVendors {
+			if requireNic == actualNic {
+				nicCount -= 1
+				break
+			}
+		}
+	}
+
+	if nicCount == 0 {
+		return true
+	} else {
+		return false
+	}
 }
